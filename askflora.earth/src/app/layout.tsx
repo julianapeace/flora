635d6f20e95699ca9bd4faca1b5fc72bb03769f2
julianapeace <chancecordelia@gmail.com>
2025-03-07 15:34:41 +0800

    added github link

--- askflora.earth/src/app/layout.tsx
+++ askflora.earth/src/app/layout.tsx
@@ -20,18 +20,37 @@ export const metadata: Metadata = {
   },
 };
 
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Quicksand:wght@300;400;500;600&display=swap" rel="stylesheet" />
+
+      </head>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
